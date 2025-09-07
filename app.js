242fc044d9a37b51811937081fad403f32770417
Suraj Haldar <163708332+surajhaldar01@users.noreply.github.com>
2025-09-07 12:55:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,81 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGamebtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+let turn0 = true; // true = Player O's turn, false = Player X's turn
+
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+    [2,5,8]
+];
+
+// Reset Game
+const resetGame = () => {
+    turn0 = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+// Add click events to each box
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if(turn0){
+            box.innerText = "O";
+            turn0 = false;
+        } else {
+            box.innerText = "X";
+            turn0 = true;
+        }
+        box.disabled = true;
+        checkWinner();   
+    });
+});
+
+// Disable all boxes
+const disabledBoxes = () => {
+    for(let box of boxes){
+        box.disabled = true;
+    }
+};
+
+// Enable all boxes
+const enableBoxes = () => {
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+    }
+};
+
+// Show winner message for both O and X
+const showWinner = (winner) => {
+    msg.innerText = ` Congratulations! Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disabledBoxes();
+};
+
+// Check winner
+const checkWinner = () => {
+    for(let pattern of winPatterns){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val !== "" && pos2Val !== "" && pos3Val !== ""){
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                showWinner(pos1Val); // Will show either X or O as winner
+                return true;
+            }
+        }
+    }
+};
+
+// Button events
+newGamebtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
